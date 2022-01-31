bb0217688e10d38cb508453c8dde2b689559cf01
shridharbavannawar <shridhar.b@razorpay.com>
2022-01-31 15:45:38 +0530

    format

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { NativeModules, Platform, NativeEventEmitter } from 'react-native';
 import { ConfigObect, TransactionResult, TransactionOutcome } from './types';
 
 const LINKING_ERROR =
-  `The package 'react-native-mpos-wrapper' doesn't seem to be linked. Make sure: \n\n` +
+  `The package 'react-native-mpos-wrapper' doesn't seem to be linked. Make sure:  \n\n` +
   Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
   '- You rebuilt the app after installing the package\n' +
   '- You are not using Expo managed workflow\n';
